5aff233cc3225f4c95f6f2edc1eb1e182819b6b4
Sam6ist <115704239+Sam6ist@users.noreply.github.com>
2024-10-24 11:54:09 +0000

    yangiladim

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('./index.html')
+    # return 'Hello world'
 
 @app.route('/content')
 def content():
-    return render_template('content.html')
+    return render_template('./content.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
