17eae8c916ee285737c7cf0a3519e678df965cf6
Sam6ist <115704239+Sam6ist@users.noreply.github.com>
2024-10-24 11:35:28 +0000

    py_yaratildi

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/content')
+def content():
+    return render_template('content.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
